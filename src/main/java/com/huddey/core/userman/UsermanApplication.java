480f50daa1e25528fdaee949eac37279b3eec344
Hayk Gasparyan <dev@jcraft.dev>
2024-11-20 01:00:45 +0100

    Initial commit

---
+++ src/main/java/com/huddey/core/userman/UsermanApplication.java
@@ -0,0 +1,13 @@
+package com.huddey.core.userman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsermanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huddey/core/userman/UsermanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huddey.core.userman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
