c776661fb5cf9ea613a8b668d36e5d524d093f0f
Hayk Gasparyan <admin@jcraft.dev>
2024-11-27 23:13:07 +0100

    Add spotless

--- src/test/java/com/huddey/core/userman/UsermanApplicationTests.java
+++ src/test/java/com/huddey/core/userman/UsermanApplicationTests.java
@@ -8,8 +8,6 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class UsermanApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {}
 }
