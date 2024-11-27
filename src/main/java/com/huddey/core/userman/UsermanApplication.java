1cb5bc4041e2d452cdcfca049df11084bc846181
JCraft <admin@jcraft.dev>
2024-11-27 23:20:53 +0100

    Configs (#16)
    * Add spotless
    
    * Add spotless

--- src/main/java/com/huddey/core/userman/UsermanApplication.java
+++ src/main/java/com/huddey/core/userman/UsermanApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UsermanApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UsermanApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(UsermanApplication.class, args);
+  }
 }
