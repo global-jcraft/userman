a2f351f8b2133c4fe374d3ad76f1be48009cd373
Hayk Gasparyan <admin@jcraft.dev>
2024-12-08 13:21:35 +0100

    DB setup
    Pipeline fix for tests

--- src/test/java/com/huddey/core/userman/UsermanServiceTests.java
+++ src/test/java/com/huddey/core/userman/UsermanServiceTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class UsermanServiceTests {
+class UsermanServiceTests extends UsermanTestConfig {
 
   @Test
   void contextLoads() {
