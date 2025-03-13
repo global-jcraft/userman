970191bdcbc6f011fe308b54869c3ee7ee0463cf
Hayk Gasparyan <admin@jcraft.dev>
2025-03-14 00:04:51 +0100

    OAuth2 implementation
    Google auth

--- src/main/java/com/huddey/core/userman/auth/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/huddey/core/userman/auth/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.huddey.core.userman.auth.JwtTokenProvider;
@@ -29,7 +30,6 @@ import com.huddey.core.userman.utils.RequestUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
