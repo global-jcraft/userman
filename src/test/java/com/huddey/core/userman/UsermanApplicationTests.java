a9c7240fd2df4c8a4145dfc14ab0647b5c9a531f
Hayk Gasparyan <admin@jcraft.dev>
2025-03-29 18:54:21 +0100

    Add tests
    Added controller, utils, mapper test,

---
+++ src/test/java/com/huddey/core/userman/UsermanApplicationTests.java
@@ -0,0 +1,12 @@
+package com.huddey.core.userman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
