07fa06be92943c5ffdb0685a307972aead8f555e
Hayk Gasparyan <admin@jcraft.dev>
2025-04-21 16:01:48 +0200

    Add Spring Boot dependencies and configure JPA repositories

--- bootstrap/src/main/java/com/huddey/main/bootstrap/BootstrapApplication.java
+++ bootstrap/src/main/java/com/huddey/main/bootstrap/BootstrapApplication.java
@@ -2,8 +2,14 @@ package com.huddey.main.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.huddey.userman.repository")
+@EntityScan(basePackages = "com.huddey.userman.data.entity")
+@ComponentScan(basePackages = "com.huddey.userman")
 public class BootstrapApplication {
 
   public static void main(String[] args) {
