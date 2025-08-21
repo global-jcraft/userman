5bc57223d10e78935a1c0554da518a1ab05159fc
Hayk Gasparyan <admin@jcraft.dev>
2025-08-21 20:35:51 +0200

    Add new Teams and Teams Pro plans with pricing for multiple seat options

--- src/main/java/com/huddey/core/payment/service/StripeSetupService.java
+++ src/main/java/com/huddey/core/payment/service/StripeSetupService.java
@@ -14,15 +14,21 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class StripeSetupService {
 
+  private static final String MONTH = "month";
+  private static final String YEAR = "year";
+  private static final String TEAMS_PLAN = "Teams Plan";
+  private static final String TEAMS_PRO_PLAN = "Teams Pro Plan";
+
   public void createHuddeyProducts() throws StripeException {
+
     // Create Starter Plan Product
     Product starterProduct =
         createProduct(
             "Starter Plan",
             "Perfect for creators who are just starting out and want to focus on automating their social media presence.");
 
-    createPrice(starterProduct.getId(), 1500L, "month", "Starter Monthly");
-    createPrice(starterProduct.getId(), 15000L, "year", "Starter Yearly");
+    createPrice(starterProduct.getId(), 1500L, MONTH, "Starter Monthly");
+    createPrice(starterProduct.getId(), 15300L, YEAR, "Starter Yearly");
 
     // Create Pro Plan Product
     Product proProduct =
@@ -30,8 +36,8 @@ public class StripeSetupService {
             "Pro Plan",
             "Designed for creators seeking to expand their reach, optimize performance through advanced analytics, and receive personalized guidance.");
 
-    createPrice(proProduct.getId(), 3500L, "month", "Pro Monthly");
-    createPrice(proProduct.getId(), 35000L, "year", "Pro Yearly");
+    createPrice(proProduct.getId(), 3900L, MONTH, "Pro Monthly");
+    createPrice(proProduct.getId(), 37400L, YEAR, "Pro Yearly");
 
     // Create Elite Plan Product
     Product eliteProduct =
@@ -39,8 +45,74 @@ public class StripeSetupService {
             "Elite Plan",
             "For established content creators, influencers, and businesses with a large presence across multiple platforms.");
 
-    createPrice(eliteProduct.getId(), 8500L, "month", "Elite Monthly");
-    createPrice(eliteProduct.getId(), 85000L, "year", "Elite Yearly");
+    createPrice(eliteProduct.getId(), 9900L, MONTH, "Elite Monthly");
+    createPrice(eliteProduct.getId(), 89100L, YEAR, "Elite Yearly");
+
+    // Create Teams Plan Product
+    Product teamsProduct10Seats =
+        createProduct(
+            TEAMS_PLAN,
+            "Ideal for agencies & brand teams that need collaboration & client management up to 10 seats.");
+
+    createPrice(teamsProduct10Seats.getId(), 24900L, MONTH, "Teams Monthly for 10 seats");
+    createPrice(teamsProduct10Seats.getId(), 253900L, YEAR, "Teams Yearly for 10 seats");
+
+    Product teamsProduct20Seats =
+        createProduct(
+            TEAMS_PLAN,
+            "Ideal for agencies & brand teams that need collaboration & client management up to 20 seats.");
+
+    createPrice(teamsProduct20Seats.getId(), 42900L, MONTH, "Teams Monthly for 20 seats");
+    createPrice(teamsProduct20Seats.getId(), 437500L, YEAR, "Teams Yearly for 20 seats");
+
+    Product teamsProduct30Seats =
+        createProduct(
+            TEAMS_PLAN,
+            "Ideal for agencies & brand teams that need collaboration & client management up to 30 seats.");
+
+    createPrice(teamsProduct30Seats.getId(), 59900L, MONTH, "Teams Monthly for 30 seats");
+    createPrice(teamsProduct30Seats.getId(), 610900L, YEAR, "Teams Yearly for 30 seats");
+
+    Product teamsProduct50Seats =
+        createProduct(
+            TEAMS_PLAN,
+            "Ideal for agencies & brand teams that need collaboration & client management up to 50 seats.");
+
+    createPrice(teamsProduct50Seats.getId(), 89900L, MONTH, "Teams Monthly for 50 seats");
+    createPrice(teamsProduct50Seats.getId(), 916900L, YEAR, "Teams Yearly for 50 seats");
+
+    // Create Teams Pro Plan Product
+    Product teamsProProduct10Seats =
+        createProduct(
+            TEAMS_PRO_PLAN,
+            "Ideal for large agencies & enterprises managing multiple clients & regions up to 10 seats.");
+
+    createPrice(teamsProProduct10Seats.getId(), 42900L, MONTH, "Teams Monthly for 10 seats");
+    createPrice(teamsProProduct10Seats.getId(), 423300L, YEAR, "Teams Yearly for 10 seats");
+
+    Product teamsProProduct20Seats =
+        createProduct(
+            TEAMS_PRO_PLAN,
+            "Ideal for large agencies & enterprises managing multiple clients & regions up to 20 seats.");
+
+    createPrice(teamsProProduct20Seats.getId(), 64500L, MONTH, "Teams Monthly for 20 seats");
+    createPrice(teamsProProduct20Seats.getId(), 642400L, YEAR, "Teams Yearly for 20 seats");
+
+    Product teamsProProduct30Seats =
+        createProduct(
+            TEAMS_PRO_PLAN,
+            "Ideal for large agencies & enterprises managing multiple clients & regions up to 30 seats.");
+
+    createPrice(teamsProProduct30Seats.getId(), 84500L, MONTH, "Teams Monthly for 30 seats");
+    createPrice(teamsProProduct30Seats.getId(), 841600L, YEAR, "Teams Yearly for 30 seats");
+
+    Product teamsProProduct50Seats =
+        createProduct(
+            TEAMS_PRO_PLAN,
+            "Ideal for large agencies & enterprises managing multiple clients & regions up to 50 seats.");
+
+    createPrice(teamsProProduct50Seats.getId(), 118500L, MONTH, "Teams Monthly for 50 seats");
+    createPrice(teamsProProduct50Seats.getId(), 1180200L, YEAR, "Teams Yearly for 50 seats");
   }
 
   private Product createProduct(String name, String description) throws StripeException {
